36bff3f722a85cd897208bf7f386664f308153b4
Mrpotatoe22 <726318@student.dusd.net>
2025-04-28 10:20:35 -0700

    Finished main part of project
    Working on challenges

--- P2SoundAndEffectSanchez/Assets/Scripts/SpawnManager.cs
+++ P2SoundAndEffectSanchez/Assets/Scripts/SpawnManager.cs
@@ -4,31 +4,28 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(25,0 , 0);
-    private float startDelay = 2;
-    private float repeatRate = 2;
+    public GameObject[] obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    public float startDelay = 2;
+    public float repeatRate = 2;
     private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("SpawnRandomObstacle", startDelay, repeatRate);
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
-    void Update()
+    void SpawnRandomObstacle()
     {
-        
-    }
-
-    void SpawnObstacle ()
-    {
-        if(playerControllerScript.gameOver == false)
+        if (playerControllerScript.gameOver == false)
         {
-            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
-        }
+            int obstacleIndex = Random.Range(0, obstaclePrefab.Length);
+            Vector3 SpawnManager = spawnPos;
 
+            Instantiate(obstaclePrefab[obstacleIndex], spawnPos, obstaclePrefab[obstacleIndex].transform.rotation);
+        }
     }
 }
